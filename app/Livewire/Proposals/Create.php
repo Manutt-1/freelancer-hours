06137ba52cbd55c0bf0af1ed47aa5354280871b5
Manutt-1 <manutt577@gmail.com>
2024-10-10 17:26:59 -0300

    timer do projeto

--- app/Livewire/Proposals/Create.php
+++ app/Livewire/Proposals/Create.php
@@ -10,7 +10,7 @@ class Create extends Component
 {
 
     public Project $project;
-    public bool $modal = true;
+    public bool $modal = false;
 
     #[Rule(['required', 'email'])]
     public string $email = '';
